d4751a52e1ebed27938fe0745e2e0ab76620512f
Cypress707 <rydertaylor707@gmail.com>
2024-12-02 15:14:21 -0600

    added the movement for the player
    added the horizontal, vertical and diagonal movement for the player, successfully this time

--- terraria boss/sketch.js
+++ terraria boss/sketch.js
@@ -19,9 +19,11 @@ let textySize = 100;
 let colourly = 255;
 let colour1 = 0;
 let backGround;
-let x;
-let y;
+let x = 0;
+let y = 0;
 let b = 1;
+let f = 1;
+let keysPressed = [];
 function setup() {
   backGround = loadImage("assets/giphy.webp");
   vyke = loadImage("assets/25b38ek4hu191.jpg");
@@ -208,8 +210,8 @@ function mousePressed() {
     title = false;
     gameStart = true;
     backGround = loadImage("assets/cloudcastle.jpg");
-    
-    
+
+
   }
   if (titleScreen === true && mouseX > width / 2 - 200 && mouseX < width / 2 + 200 && mouseY > height / 2 - 100 && mouseY < height / 2 + 100) {
     characterSelect = true;
@@ -257,38 +259,64 @@ function selectCharacter() {
   }
 }
 
-function player(x,y){
-  if (gameStart === false){
-    x = width/2;
-    y = height/2;
-  }
-  if(gameStart === true){
-    print(x,y);
-    
-    circle(x,y,5);
-    if(keyIsPressed && key === 'a'){
-      print("hello");
-      if(x > 0){
-        print("hi");
-        // if(key === 'w'){
-        //   if(y < 0){
-        //     x -= 1;
-        //     y -= 1;
-        //   }
-        // }
-        // else if (key === 's'){
-        //   if(y > height){
-        //     x -= 1;
-        //     y += 1;
-        //   }
-        // }
-        // else{
-          x -= 1;
-        //}
-      } 
+function player() {
+  if (gameStart === true) {
+    if (f === 1) {
+      print(x, y);
+      x = width / 2;
+      y = height / 2;
+      f = 0;
     }
-    
-  }
-  
-}
 
+    circle(x, y, 5);
+    if (keyIsDown(65)) {
+      keysPressed[0] = 1;
+      x -= 4;
+    }
+    if (keyIsDown(87) && keysPressed[0] === 1) {
+      x -= 1;
+      y -= 1;
+    }
+    else if (keyIsDown(83) && keysPressed[0] === 1) {
+      x -= 1;
+      y += 1;
+    }
+    if (keyIsDown(68)) {
+      keysPressed[0] = 4;
+      x += 4;
+    }
+    if (keyIsDown(87) && keysPressed[0] === 4) {
+      x += 1;
+      y -= 1;
+    }
+    else if (keyIsDown(83) && keysPressed[0] === 4) {
+      x += 1;
+      y += 1;
+    }
+    if (keyIsDown(87)) {
+      keysPressed[0] = 2;
+      y -= 4;
+    }
+    if (keyIsDown(65) && keysPressed[0] === 2) {
+      x -= 1;
+      y -= 1;
+    }
+    else if (keyIsDown(68) && keysPressed[0] === 2) {
+      x += 1;
+      y -= 1;
+    }
+    if (keyIsDown(83)) {
+      keysPressed[0] = 3;
+      y += 4;
+    }
+    if (keyIsDown(65) && keysPressed[0] === 3) {
+      x -= 1;
+      y += 1;
+    }
+    else if (keyIsDown(68) && keysPressed[0] === 3) {
+      x += 1;
+      y += 1;
+    }
+    keysPressed[0] = 0;
+  }
+}
