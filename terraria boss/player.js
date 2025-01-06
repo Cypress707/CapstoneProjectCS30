b15cc9cbcb744cabac35cb47a5ad001bc8ca5a48
Cypress707 <rydertaylor707@gmail.com>
2025-01-06 15:20:17 -0600

    added the goku sprite and character class
    made it easier to update character and added the sprites for the player

--- terraria boss/player.js
+++ terraria boss/player.js
@@ -1,8 +1,71 @@
 class Player {
-  constructor(x,y){
-    player.x = x;
-    player.y = y;
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
   }
+  move() {
+    if (gameStart === true) {
+      if (f === 1) {
+        //print(x, y);
+        this.x = width / 2;
+        this.y = height / 2;
+        f = 0;
+      }
+
+      circle(this.x, this.y, 6);
+      if (keyIsDown(65)) {
+        keysPressed[0] = 1;
+        this.x -= 4;
+      }
+      if (keyIsDown(87) && keysPressed[0] === 1) {
+        this.x -= 1;
+        this.y -= 1;
+      }
+      else if (keyIsDown(83) && keysPressed[0] === 1) {
+        this.x -= 1;
+        this.y += 1;
+      }
+      if (keyIsDown(68)) {
+        keysPressed[0] = 4;
+        this.x += 4;
+      }
+      if (keyIsDown(87) && keysPressed[0] === 4) {
+        this.x += 1;
+        this.y -= 1;
+      }
+      else if (keyIsDown(83) && keysPressed[0] === 4) {
+        this.x += 1;
+        this.y += 1;
+      }
+      if (keyIsDown(87)) {
+        keysPressed[0] = 2;
+        this.y -= 4;
+      }
+      if (keyIsDown(65) && keysPressed[0] === 2) {
+        this.x -= 1;
+        this.y -= 1;
+      }
+      else if (keyIsDown(68) && keysPressed[0] === 2) {
+        this.x += 1;
+        this.y -= 1;
+      }
+      if (keyIsDown(83)) {
+        keysPressed[0] = 3;
+        this.y += 4;
+      }
+      if (keyIsDown(65) && keysPressed[0] === 3) {
+        this.x -= 1;
+        this.y += 1;
+      }
+      else if (keyIsDown(68) && keysPressed[0] === 3) {
+        this.x += 1;
+        this.y += 1;
+      }
+      keysPressed[0] = 0;
+    }
+
+  }
+
 }
 
 
--- terraria boss/sketch.js
+++ terraria boss/sketch.js
@@ -51,9 +51,16 @@ let beamX = 75;
 let beamY = 50;
 let beamH = 200;
 let beamW = 10;
+let player;
 let attack = true;
+let goku = [];
+let spriteGoku = 0;
 function setup() {
+  player = new Player(x,y)
   backGround = loadImage("assets/giphy.webp");
+  for (let i = 0; i < 5; i ++){
+    goku.push(loadImage("assets/gokubase" + i + ".png"));
+  }
   for (let i = 0; i < 50; i++) {
     broly.push(loadImage("assets/brolyBasessj" + i + ".png"));
     print(i);
@@ -77,14 +84,17 @@ function setup() {
 }
 
 function draw() {
+  image(goku[gokuSprite],width/2,height/2)
   background(backGround);
   backgroundTitle();
 
   drawTitleScreen();
   pressStart();
   selectCharacter();
-  player();
+  //player();
+  player.move();
   boss();
+  
 }
 
 
@@ -301,74 +311,74 @@ function selectCharacter() {
   }
 }
 
-function player(x, y) {
-  if (gameStart === true) {
-    if (f === 1) {
-      //print(x, y);
-      x = width / 2;
-      y = height / 2;
-      f = 0;
-    }
+// function player() {
+//   if (gameStart === true) {
+//     if (f === 1) {
+//       //print(x, y);
+//       x = width / 2;
+//       y = height / 2;
+//       f = 0;
+//     }
 
-    circle(x, y, 6);
-    if (keyIsDown(65)) {
-      keysPressed[0] = 1;
-      x -= 4;
-    }
-    if (keyIsDown(87) && keysPressed[0] === 1) {
-      x -= 1;
-      y -= 1;
-    }
-    else if (keyIsDown(83) && keysPressed[0] === 1) {
-      x -= 1;
-      y += 1;
-    }
-    if (keyIsDown(68)) {
-      keysPressed[0] = 4;
-      x += 4;
-    }
-    if (keyIsDown(87) && keysPressed[0] === 4) {
-      x += 1;
-      y -= 1;
-    }
-    else if (keyIsDown(83) && keysPressed[0] === 4) {
-      x += 1;
-      y += 1;
-    }
-    if (keyIsDown(87)) {
-      keysPressed[0] = 2;
-      y -= 4;
-    }
-    if (keyIsDown(65) && keysPressed[0] === 2) {
-      x -= 1;
-      y -= 1;
-    }
-    else if (keyIsDown(68) && keysPressed[0] === 2) {
-      x += 1;
-      y -= 1;
-    }
-    if (keyIsDown(83)) {
-      keysPressed[0] = 3;
-      y += 4;
-    }
-    if (keyIsDown(65) && keysPressed[0] === 3) {
-      x -= 1;
-      y += 1;
-    }
-    else if (keyIsDown(68) && keysPressed[0] === 3) {
-      x += 1;
-      y += 1;
-    }
-    keysPressed[0] = 0;
-    playerPos = (x, y, 6);
-  }
+//     circle(x, y, 6);
+//     if (keyIsDown(65)) {
+//       keysPressed[0] = 1;
+//       x -= 4;
+//     }
+//     if (keyIsDown(87) && keysPressed[0] === 1) {
+//       x -= 1;
+//       y -= 1;
+//     }
+//     else if (keyIsDown(83) && keysPressed[0] === 1) {
+//       x -= 1;
+//       y += 1;
+//     }
+//     if (keyIsDown(68)) {
+//       keysPressed[0] = 4;
+//       x += 4;
+//     }
+//     if (keyIsDown(87) && keysPressed[0] === 4) {
+//       x += 1;
+//       y -= 1;
+//     }
+//     else if (keyIsDown(83) && keysPressed[0] === 4) {
+//       x += 1;
+//       y += 1;
+//     }
+//     if (keyIsDown(87)) {
+//       keysPressed[0] = 2;
+//       y -= 4;
+//     }
+//     if (keyIsDown(65) && keysPressed[0] === 2) {
+//       x -= 1;
+//       y -= 1;
+//     }
+//     else if (keyIsDown(68) && keysPressed[0] === 2) {
+//       x += 1;
+//       y -= 1;
+//     }
+//     if (keyIsDown(83)) {
+//       keysPressed[0] = 3;
+//       y += 4;
+//     }
+//     if (keyIsDown(65) && keysPressed[0] === 3) {
+//       x -= 1;
+//       y += 1;
+//     }
+//     else if (keyIsDown(68) && keysPressed[0] === 3) {
+//       x += 1;
+//       y += 1;
+//     }
+//     keysPressed[0] = 0;
+//     playerPos = (x, y, 6);
+//   }
 
-}
+// }
 function boss() {
 
   if (startAttack0 === false) {
-    brolyX = width / 2;
-    brolyY = height / 2;
+    brolyX = width / 4;
+    brolyY = height / 4;
   }
   if (gameStart === true) {
     //print(framee);
@@ -530,7 +540,7 @@ function boss() {
             //else {
 
             //}
-            brolyY = random(200, height - 200);
+            brolyY = random(0,height);
           }
           if (framee === 20) {
             // if (brolyD === 0) {
